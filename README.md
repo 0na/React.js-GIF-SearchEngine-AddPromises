6de18400947cde533374aee87be5a83a9793aae2
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-02-26 08:36:46 +0100

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# React.js-GIF-SearchEngine
+
+https://0na.github.io/React.js-GIF-SearchEngine/
---
+++ components/Search.jsx
@@ -0,0 +1,39 @@
+Search = React.createClass({
+  getInitialState() {
+    return {
+      searchingText: ""
+    };
+  },
+
+  handleChange: function(event) {
+    var searchingText = event.target.value;
+    this.setState({ searchingText: searchingText });
+
+    if (searchingText.length > 2) {
+      this.props.onSearch(searchingText);
+    }
+  },
+
+  handleKeyUp: function(event) {
+    //metodę, która będzie rozpoznawała, że wciśnięty klawisz to Enter i również wyśle wiadomość do rodzica, aby ten jeszcze raz uruchomił funkcję wysyłającą zapytanie po gifa:
+    if (event.keyCode === 13) {
+      // Kod klawisza Enter to 13
+      this.props.onSearch(this.state.searchingText); //Jeśli wciśniemy więc Enter, to wywołana zostanie funkcja
+    }
+  },
+
+  render: function() {
+    var styles = { fontSize: "1.5em", width: "90%", maxWidth: "350px" };
+
+    return (
+      <input
+        type="text"
+        onChange={this.handleChange}
+        onKeyUp={this.handleKeyUp} //nasłuchiwanie na wciśnięcie, a właściwie na "odciśnięcie" klawisza Enter (onKeyUp):
+        placeholder="Tutaj wpisz wyszukiwaną frazę"
+        style={styles}
+        value={this.state.searchTerm}
+      />
+    );
+  }
+});
